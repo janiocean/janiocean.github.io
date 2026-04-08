26d9c2d5dedc1672841474354bd68165d078d1ed
Danil Jan <jandevguard@gmail.com>
2026-04-08 23:24:30 +0300

    V0.1 With style

--- sound.js
+++
@@ -1,6 +0,0 @@
-const audio = new Audio("files/sounds/test_active.mp3");
-const button = document.getElementById("myButton");
-
-document.getElementById("myButton").onclick = function() {
-    audio.play();
-}
