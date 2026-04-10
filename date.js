f14cb8122adcb09d10f4ef876e3fd9da7958dcda
Danil Jan <jandevguard@gmail.com>
2026-04-10 11:18:30 +0300

    Better button and flipping lang

--- date.js
+++ date.js
@@ -15,6 +15,41 @@ var getJSON = function(url, callback) {
     xhr.send();
 };
 
+let myHours = 0;
+
+function updateLastUpdateCaption() {
+    var date_text = "";
+    var hours = myHours;
+    if (hours < 24) {
+        if (currentLanguage == "ru") {
+            if (hours == 0) { date_text = "меньше часа назад"; } else
+            if (hours == 1) { date_text = "час назад"; } else
+            if (hours > 1 && hours < 5) { date_text = hours + " часа назад"; }
+            else { date_text = hours + " часов назад"; }
+        }
+        else {
+            if (hours == 0) { date_text = "hour ago"; }
+            else { date_text = hours + " hours ago"}
+        }
+    }
+    else {
+        var days = Math.floor(hours / 24);
+        if (currentLanguage == "ru") {
+            if (days == 1) { date_text = "день назад"; } else
+            if (days > 1 && days < 5) { date_text = days + " дня назад"; }
+            else { date_text = days + " дней назад"}
+        }
+        else {
+            if (days == 1) { date_text = "day ago"; }
+            else { date_text = days + " days ago"; }
+        }
+    }
+    if (currentLanguage == "ru")
+        document.getElementById("last_update_label").innerHTML = "<b>Последнее обновление было :/</b> " + date_text;
+    else
+        document.getElementById("last_update_label").innerHTML = "<b>Last update was :/</b> " + date_text;
+}
+
 getJSON(target_url,
     function(err, data) {
         if (err != null) {
@@ -23,21 +58,8 @@ getJSON(target_url,
             var inp = data["commit"]["commit"]["author"]["date"];
             var commit_date = new Date(inp);
             var diff_date = Date.now() - commit_date.getTime();
-            var hours = Math.floor(diff_date / (1000 * 60 * 60));
-            var date_text = "";
-            if (hours < 24) {
-                if (hours == 0) { date_text = "меньше часа назад"; } else
-                if (hours == 1) { date_text = "час назад"; } else
-                if (hours > 1 && hours < 5) { date_text = hours + " часа назад"; }
-                else { date_text = hours + " часов назад"; }
-            }
-            else {
-                var days = Math.floor(hours / 24);
-                if (days == 1) { date_text = "день назад"; } else
-                if (days > 1 && days < 5) { date_text = days + " дня назад"; }
-                else { date_text = days + " дней назад"}
-            }
-            document.getElementById('last_update_label').innerHTML += date_text;
+            myHours = Math.floor(diff_date / (1000 * 60 * 60));
+            updateLastUpdateCaption();
         }
     }
 );
--- language.js
+++ language.js
@@ -1,6 +1,18 @@
-async function setLanguage(lang) {
-    const response = await fetch("/languages/" + lang + ".json");
+let currentLanguage = "ru";
+
+function updateLastUpdateCaption() {}
+
+async function switchLanguage() {
+    if (currentLanguage == "ru")
+        currentLanguage = "en";
+    else
+        currentLanguage = "ru";
+
+    const response = await fetch("/languages/" + currentLanguage + ".json");
     const data = await response.json();
+
+    document.getElementById("language-switcher").innerText = data.language;
     document.getElementById("div1").innerText = data.data;
     document.getElementById("main-card").innerText = data.main_card;
+    updateLastUpdateCaption();
 }
