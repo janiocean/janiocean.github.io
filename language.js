13f3a524465f548ad9c7e6be05a70183b1764ce4
Danil Jan <jandevguard@gmail.com>
2026-04-10 09:51:29 +0300

    Lang switcher prot 2

--- language.js
+++ language.js
@@ -1,22 +1,6 @@
-function readTextFile(file) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState == 4 && rawFile.status == "200") {
-            return rawFile.resonseText;
-        }
-    }
-    rawFile.send(null);
-}
-
-function setLanguage(lang) {
-    if (lang == "ru") {
-        document.getElementById("div1").innerText = "РУССКАЯ ИНФА ТУТ";
-        document.getElementById("main-card").innerText = "JAN КАРТА";
-    }
-    if (lang == "en") {
-        document.getElementById("div1").innerText = "ENGLISH INFO HERE";
-        document.getElementById("main-card").innerText = "JAN CARD";
-    }
+async function setLanguage(lang) {
+    const response = await fetch("/languages/" + lang + ".json");
+    const data = await response.json();
+    document.getElementById("div1").innerText = data.data;
+    document.getElementById("main-card").innerText = data.main_card;
 }
